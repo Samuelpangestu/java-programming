55f4d9dbf5d1e388534ba36a142cef532ee09f86
Samuel Pangestu G Tobing <samuel.pangestu.gonggom@gmail.com>
2024-06-02 22:28:51 +0700

    add getterSetterPractice class

---
+++ chapter9/getterSetterPractice.java
@@ -0,0 +1,31 @@
+package chapter9;
+
+public class getterSetterPractice {
+    public String nama;
+    public float balance;
+    public int umur;
+
+    public float getBalance() {
+        return balance;
+    }
+
+    public void setBalance(float balance) {
+        this.balance = balance;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public int getUmur() {
+        return umur;
+    }
+
+    public void setUmur(int umur) {
+        this.umur = umur;
+    }
+}
